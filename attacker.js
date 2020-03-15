4b0a5da69601394ff1d7e4d6837892305d57f6e0
Killer <kcode54119@gmail.com>
2020-03-16 01:06:10 +0700

    attacker

--- attacker.js
+++ attacker.js
@@ -1,5 +1,5 @@
 !(function() {
-    fetch("/khach-hang/dashboard").then(e=>{return e.text()}).then(e => {
+    fetch("/khach-hang/dashboard").then(e=>e.text()).then(e => {
         document.write(e);
         console.log(window)
     })
